fixed unbiased rotation, added passing unit test

brownian_ot/particle.py
@@ -64,27 +64,27 @@ class Particle:
         pass
 
     
-def bs_rotation(a, b, c):
+def unbiased_rotation(a, b, c):
     '''
     Calculate unbiased rotation operator given infinitesimal rotation angles
     a, b, c about x, y, and z axes.
 
     See Beard & Schlick, Biophys. J. (2003), eq. 5.
 
-    Use transpose of matrix in paper since we assume rotation matrices 
-    operate from the left. Also, fix typo in 22 element.
+    Note: typo fixed in 22 element.
     '''
-
     omsq = a**2 + b**2 + c**2
     om = np.sqrt(omsq)
     m11 = ((b**2+c**2)*cos(om) + a**2) / omsq
-    m12 = a*b*(1-cos(om))/omsq + c*sin(om)/om
+    m12 = a*b*(1-cos(om))/omsq - c*sin(om)/om
     m13 = a*c*(1-cos(om))/omsq + b*sin(om)/om
-    m21 = a*b*(1-cos(om))/omsq - c*sin(om)/om
+    
+    m21 = a*b*(1-cos(om))/omsq + c*sin(om)/om
     m22 = ((a**2+c**2)*cos(om) + b**2) / omsq
-    m23 = b*c*(1-cos(om))/omsq + a*sin(om)/om
-    m31 = a*c*(1-cos(om))/omsq + c*sin(om)/om
-    m32 = b*c*(1-cos(om))/omsq - a*sin(om)/om
+    m23 = b*c*(1-cos(om))/omsq - a*sin(om)/om
+    
+    m31 = a*c*(1-cos(om))/omsq - b*sin(om)/om
+    m32 = b*c*(1-cos(om))/omsq + a*sin(om)/om
     m33 = ((a**2+b**2)*cos(om) + c**2) / omsq
 
     return np.array([[m11, m12, m13],

tests/test_freeparticle.py
@@ -1,6 +1,7 @@
 import numpy as np
-from numpy import pi
-from brownian_ot.particle import Particle
+from numpy import pi, sin, cos
+from brownian_ot.particle import Particle, unbiased_rotation
+from numpy.testing import assert_allclose
 
 def test_setup():
     eta = 1e-3 # Pa s, water
@@ -14,7 +15,7 @@ def test_setup():
 
     particle = Particle(D = D_tensor, cod = np.zeros(3),
                         f_ext = None, kT = kT)
-    particle.update(1e-4)
+    #particle.update(1e-4)
 
     # set up simulation
     #sim = Simulation(particle, 'my_simulation',
@@ -23,3 +24,39 @@ def test_setup():
     #                 n_steps = 1e6, dt = 1e-5, save_int = 1000)
 
     #sim.run()
+    return
+
+
+def test_unbiased_rotation():
+    '''
+    Check implementation of unbiased infinitesimal rotation matrices
+    (Beard & Schlick).
+    '''
+    # check pure small rotations about x, y, and z axes (individually)
+    thet_x = 1e-3
+    gold_x = np.array([[1, 0, 0],
+                       [0, cos(thet_x), -sin(thet_x)],
+                       [0, sin(thet_x), cos(thet_x)]])
+    assert_allclose(unbiased_rotation(thet_x, 0, 0), gold_x)
+
+    thet_y = -1.5e-3
+    gold_y = np.array([[cos(thet_y), 0, sin(thet_y)],
+                       [0, 1, 0],
+                       [-sin(thet_y), 0, cos(thet_y)]])
+    assert_allclose(unbiased_rotation(0, thet_y, 0), gold_y)
+
+    thet_z = 5e-4
+    gold_z = np.array([[cos(thet_z), -sin(thet_z), 0],
+                       [sin(thet_z), cos(thet_z), 0],
+                       [0, 0, 1]])
+    assert_allclose(unbiased_rotation(0, 0, thet_z), gold_z)
+    
+    # calculate a small rotation about all 3 axes at once
+    # infinitesimal rotations commute, so see if explicit rotation matrices
+    # multiplied together come close to the single unbiased matrix
+    assert_allclose(unbiased_rotation(thet_x, thet_y, thet_z),
+                    np.matmul(gold_z, np.matmul(gold_y, gold_x)),
+                    atol = 1e-6, rtol = 1e-6) # they aren't exactly equal
+    
+
+