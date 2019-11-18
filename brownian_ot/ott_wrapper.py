ae417d7b4fd436a928a5bd45c1ef36c4e4b44b12
Jerome Fung <jfung@ithaca.edu>
2019-11-18 01:01:58 -0500

    force from force factory runs, not yet validated, need to correctly normalize force/torque for incident beam power

--- brownian_ot/ott_wrapper.py
+++ brownian_ot/ott_wrapper.py
@@ -20,6 +20,39 @@ eng.addpath(config['matlab_wrapper_path'])
 
 # TODO: unit conversion from efficiencies to real forces
 
-def make_optical_force():
-    return
+def make_sphere_force(lambda_0, pol, n_med, NA, power, n_p, r_p):
+    '''
+    Factory to return a function to calculate the optical force
+    on a sphere.
+
+    Inputs:
+    lambda_0 : vacuum wavelength
+    pol : 2-element list/ndarray (x and y components)
+    n_med : medium index
+    NA : objective numerical aperture
+    n_p : particle index
+    r_p : particle radius
+    '''
+
+    eng.ott_beam(lambda_0, pol[0], pol[1], NA, n_med, nargout = 0)
+    eng.ott_tmatrix_sphere(n_p, r_p, lambda_0, n_med, nargout = 0)
+
+    def force(pos, rot_matrix):
+        if isinstance(pos, np.ndarray):
+            pos = pos.tolist()
+
+        pos = matlab.double(pos)
+
+        if isinstance(rot_matrix, np.ndarray):
+            rot_matrix = rot_matrix.tolist()
+
+        rot_matrix = matlab.double(rot_matrix)
+
+        fx, fy, fz, tx, ty, tz = eng.ott_calc_force(pos, rot_matrix,
+                                                    nargout = 6)
+        # TODO: fix normalization
+        return np.array([fx, fy, fz, tx, ty, tz])
+        
+        
+    return force
 
