29c2ad00cbca9c0ae38c4e5ba2ab5c918703f7d1
Jerome Fung <jfung@ithaca.edu>
2020-05-21 18:37:40 -0400

    new ideas for interface

--- mockup.py
+++ mockup.py
@@ -3,9 +3,64 @@ Mock up the use case for running a Brownian dynamics simulation of particle
 in OT.
 '''
 
-particle = Spheroid(a = 0.2e-6, ar = 1.5, refractive_index = 1.5,
-                    viscosity = 1e-3, kT = 295 * 1.38e-23,
-                    seed = 12345678, pos = np.array([0, 0, 1.04103e-7]))
+# Use case #1: Brownian OT for a spheroid
+particle1 = Spheroid(a = 0.2e-6, ar = 1.5, refractive_index = 1.5)
+# Spheroids could have some methods like equiv_sphere_radius
+# or, a sphere cluster might have some specific attributes or similar methods
+# particle needs its own center of diffusion.
+# But I'm less clear that Particle should have a position or orientation
+# attribute
+
+# TODO: make Beam a class.
+# This way, init can make beam.pol a "legal" python list whether you pass
+# in a list, a tuple, or an ndarray
+beam = Beam(wavelen = 1064e-9,
+            pol = np.array([1, 1j]), # This should be legal!
+            NA = 1.2,
+            medium_index = 1.33,
+            power = 5e-3)
+
+sim = OTSimulation(particle1, beam, timestep = 1e-5,
+                   viscosity = 1e-3,
+                   kT = 295 * 1.38e-23, seed = 1234578,
+                   pos0 = np.array([0, 0, 1.04103e-7]),
+                   orient0 = np.identity(3))
+
+# It's only when simulating that kT matters.
+# Ditto for viscosity, or rng seed, or initial position/orientation
+# Or maybe the particle should have its own position, orientation
+# because they're really a property of the particle.
+# But I would make them private.
+
+sim.run(1000000, 'my_simulation_results')
+
+# TODO: code should catch if there's a .npy extension
+
+
+# Use case #2: force calculation
+force_func = make_ots_force(particle, beam) # associated w/specific particle and beam
+zs = np.linspace(-2e-6, 2e6, 201)
+f_z = calc_fz(zs, force_func) # TODO: implement convenience function
+zeq = find_zeq(force_func) # TODO: implement convenience function
+xs = np.linspace(-3e-6, 3e-6, 301)
+f_x = calc_fx(xs, force_func, z = zeq) # TODO: implement convenience function
+
+
+# Use case #3: a simulation with free diffusion
+particle2 = Sphere(a = particle1.equivalent_radius())
+# have a default refractive index of None, raise ValueError?
+sim2 = FreeDiffusionSim(particle2, timestep = 5e-4, viscosity = 1e-3,
+                        kT = 295 * 1.38e-23, seed = 987654321)
+# default position0, orient0
+traj = sim2.run(10000) # don't save this automatically
+
+
+
+
+
+
+
+
 
 # use a NamedTuple for this.
 # Python 3.7 has a DataClass object, which might actually be great for this,
@@ -14,11 +69,6 @@ particle = Spheroid(a = 0.2e-6, ar = 1.5, refractive_index = 1.5,
 # But a class seems like overkill: I don't think this will have any
 # non-trivial methods. It's going to get passed to ott
 # to create an ott beam object, where a lot of math happens)
-beam = Beam(wavelen = 1064e-9,
-            pol = np.array([1, 1j]),
-            NA = 1.2,
-            medium_index = 1.33,
-            power = 5e-3)
 
 # The main reason to make Simulation a class is to allow for inheritance.
 # There is a base Simulation class
@@ -27,15 +77,7 @@ beam = Beam(wavelen = 1064e-9,
 # By subclassing Simulation, and attaching a particular force model to it,
 # the user doesn't need to explicitly construct the force.
 
-sim = OTSimulation(particle, beam, timestep = 1e-5)
-
-sim.run(1000000, 'my_simulation_results')
 
 
-# Also, mock up a force calculation
-force_func = make_ots_force(particle, beam)
-zs = np.linspace(-2e-6, 2e6, 201)
-f_z = calc_fz(zs, force_func) # TODO: implement convenience function
-zeq = find_zeq(force_func) # TODO: implement convenience function
-xs = np.linspace(-3e-6, 3e-6, 301)
-f_x = calc_fx(xs, force_func, z = zeq) # TODO: implement convenience function
+
+
